ceb92f7803086441d021119930dae4e8e1e5c063
orest.ivanychuk <orestysorestys@gmail.com>
2017-11-13 09:36:21 +0200

    fixed deprecation

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import cors from 'cors';
 import bodyParser from  'body-parser'
 import mongoose from 'mongoose'
 
-let port = process.env.PORT || 3000
-const mongoUrl = process.env.MONGOLAB_URI || process.env.MONGODB_URI;
+const port = process.env.PORT || 3000
+const mongoUrl = process.env.MONGOLAB_URI || process.env.MONGODB_URI || 'mongodb://localhost:27017/test';
 
 mongoose.connect( mongoUrl, { useMongoClient: true })
-mongoose.connection.on('error',(err) => console.log("Error when connecting to mongo"));
-console.log(mongoUrl)
+mongoose.connection.on('error',(err) => console.log("Error when connecting to mongo"))
+mongoose.connection.on('connected',() => console.log('Connection successfully established'))
 
-let app = express()
+console.log(process.env)
+
+const app = express()
 app.server = http.createServer(app)
 
 app.use(bodyParser.json());
